0df36faa6ddf5957ba227b3c6192edad31d6bc0c
nonishenko <onishenko@ebola.com.ua>
2015-06-26 18:01:21 +0300

    start lesson-2

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
+"use strict";
+
 var gulp = require('gulp'),
     concatCss = require('gulp-concat-css'),
+    prefixer = require('gulp-autoprefixer'),
     minifyCss = require('gulp-minify-css'),
     rename = require('gulp-rename'),
     notify = require('gulp-notify');
@@ -7,6 +10,7 @@ var gulp = require('gulp'),
 gulp.task('default', function(){
     gulp.src('dev/css/*.css') // откуда берем исходные css-файлы для конкатанации
         .pipe(concatCss('style.css')) // как будет называться новый файл
+        .pipe(prefixer('last 2 versions', '> 1%', 'ie 9'))
         .pipe(minifyCss()) // минифицируем файлы
         .pipe(rename('style.min.css')) // переименовываем файл после минификации
         .pipe(gulp.dest('assets/css/')) // куда сохранить новый файл
